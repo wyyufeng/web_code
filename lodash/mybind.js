10726d833a2b99b19ecf6c2fa763d65bdde31496
wyyufeng <wyyufeng@gmail.com>
2021-03-23 10:43:03 +0800

    修改bind的实现

--- lodash/mybind.js
+++ lodash/mybind.js
@@ -1,18 +1,15 @@
-function mybind() {
-  const args = Array.prototype.slice.call(arguments, 1);
-  const ctx = arguments[0];
+function mybind(scope, ...args) {
   // 因为bind的调用方式就是  fn.bind  所以bind的this  就是函数本身
-  const target = this;
-  if (typeof target !== 'function') {
+  if (typeof this !== 'function') {
     return TypeError('');
   }
-  const boundFn = function () {
+  const boundFn = function (...rest) {
     return target.apply(
       // 如果使用 new 实例化  要忽略传递的this值
       // 此时的this会是 boundFn 的实例(new 的过程如此)
-      this instanceof boundFn ? this : ctx,
+      this instanceof boundFn ? this : scope,
       // 这里要注意可能返回的函数也会有参数
-      args.concat(Array.prototype.slice.call(argumenmts))
+      [...args, ...rest]
     );
   };
   // 如果使用new关键词  则新创建的实例和原来的函数原型之间没有关系了 因此需要继承原函数
@@ -20,6 +17,5 @@ function mybind() {
   const f = function () {};
   f.prototype = this.prototype;
   boundFn.prototype = new f();
-
   return boundFn;
 }
